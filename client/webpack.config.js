a1b06c51997c949c455e06c5bcc278ca56acbd5b
cbazil114 <113719887+cbazil114@users.noreply.github.com>
2023-02-20 18:28:07 -0500

    testing something

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -64,7 +64,7 @@ module.exports = () => {
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
-          // We use babel-loader in order to use ES6.
+          // We use babel-loader in order to use ES6. 
           use: {
             loader: 'babel-loader',
             options: {
