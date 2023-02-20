04da4e246841a0cd173af2f9227c8a156ae24180
cbazil114 <113719887+cbazil114@users.noreply.github.com>
2023-02-20 15:54:18 -0500

    Trying to fix issue. Might be computer and not code

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -24,7 +24,7 @@ module.exports = () => {
             // Webpack plugin that generates our html file and injects our bundles. 
             new HtmlWebpackPlugin({
               template: './index.html',
-              title: 'Contact Cards'
+              title: 'Text Editor'
             }),
            
             // Injects our custom service worker
@@ -37,9 +37,9 @@ module.exports = () => {
             new WebpackPwaManifest({
               fingerprints: false,
               inject: true,
-              name: 'Contact Cards',
-              short_name: 'Contact',
-              description: 'Never forget your contacts!',
+              name: 'Text Editor',
+              short_name: 'Editor',
+              description: 'Save your texts and save them at will!',
               background_color: '#225ca3',
               theme_color: '#225ca3',
               start_url: './',
